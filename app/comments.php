8e9bd72d9e8f5dc80126b97eb932b3e32874b72f
Khmelevskiy25 <Parkour22@bigmir.net>
2019-11-02 14:57:44 +0200

    first commit

---
+++ app/comments.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+<?php
+ $commentsUsers = [
+    [
+        "name" => "Alex",
+        "date" => date("d/m/Y"),
+        "text" => "test"
+    ],
+    [
+        "name" => "Igor",
+        "date" => date("d/m/Y"),
+        "text" => "test_1"
+    ],
+    [
+        "name" => "Sasha",
+        "date" => date("d/m/Y"),
+        "text" => "test_2"
+    ]
+];
+
+?>
+
+<?php include 'footer.php'; ?>
